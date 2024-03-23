07687b6d5747657d0aee45caf4758bbb153ef3b1
polya2005 <65003790+polya2005@users.noreply.github.com>
2024-03-23 09:48:24 -0400

    initial commit

---
+++ SentimentValueTable.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SentimentValueTable {
+    private HashMap<String, Integer> sentimentValues = new HashMap<>();
+
+    /**
+     * Creates the <code>SentimentValueTable</code> from text file in the
+     * format<br/>
+     * word [tab] sentiment<br/>
+     * word [tab] sentiment<br/>
+     * ...<br/>
+     * word [tab] sentiment<br/>
+     * 
+     * @param filename name of the file containing the lyrics
+     */
+    public SentimentValueTable(String filename){
+        Scanner sentimentReader;
+        try {
+            sentimentReader = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.err.println("Sentiment table not found: " + filename);
+            e.printStackTrace();
+            return;
+        }
+        while (sentimentReader.hasNextLine()) {
+            String line = sentimentReader.nextLine();
+            sentimentValues.put(line.substring(0, line.indexOf('\t')), Integer.parseInt(line.substring(line.indexOf('\t') + 1)));
+        }
+    }
+
+    /**
+     * @param word
+     * @return the sentiment of <code>word</code>
+     */
+    public int getSentiment(String word){
+        return sentimentValues.getOrDefault(word, 0);
+    }
+}
